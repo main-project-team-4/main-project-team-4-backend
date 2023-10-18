4eeb8e09de3e128edb552bd0aaa07e56e15f51ff
iksadNorth <iksadNorth@gmail.com>
2023-10-18 16:57:26 +0900

    "Feat : 상점명 변경 시, Unique 제한조건 설정"
    상점명 변경 시, Unique 제한조건 설정

--- src/main/java/com/example/demo/shop/repository/ShopRepository.java
+++ src/main/java/com/example/demo/shop/repository/ShopRepository.java
@@ -3,6 +3,9 @@ package com.example.demo.shop.repository;
 import com.example.demo.shop.entity.Shop;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ShopRepository extends JpaRepository<Shop, Long> {
 
+    Optional<Shop> findByShopName(String shopName);
 }
