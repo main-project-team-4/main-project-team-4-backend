bd6fde1f4a191a6b0f86c5a5d74646eb064380a2
iksadNorth <iksadNorth@gmail.com>
2023-10-19 17:43:16 +0900

    "Fix : 인증 오류 시, 로깅"
    인증 오류 시, 로깅

--- src/main/java/com/example/demo/exception/AuthenticationEntryPointImpl.java
+++ src/main/java/com/example/demo/exception/AuthenticationEntryPointImpl.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
@@ -23,6 +25,8 @@ public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
             HttpServletResponse response,
             AuthenticationException authException
     ) throws IOException {
+        log.info(request.getHeader("Authorization"));
+
         String message = "인증 오류 발생";
         HttpStatus httpStatus = HttpStatus.UNAUTHORIZED;
 
