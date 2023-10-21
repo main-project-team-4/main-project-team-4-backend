b028a34010503c44ccb26c4cc93b168d9c14a52f
iksadNorth <iksadNorth@gmail.com>
2023-10-21 13:18:50 +0900

    "Fix : 쓸모 없는 구현체 삭제"
    쓸모 없는 구현체 삭제

--- src/main/java/com/example/demo/follow/repository/FollowRepository.java
+++ src/main/java/com/example/demo/follow/repository/FollowRepository.java
@@ -4,11 +4,9 @@ import com.example.demo.follow.entity.Follow;
 import com.example.demo.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Iterator;
-import java.util.List;
 import java.util.Optional;
 
-public interface FollowRepository extends JpaRepository<Follow, Long>, BatchFollowRepository {
+public interface FollowRepository extends JpaRepository<Follow, Long> {
     boolean existsByMemberAndShop_Id(Member principal, Long shopId);
 
     Optional<Follow> findByMemberAndShop_Id(Member principal, Long shopId);
