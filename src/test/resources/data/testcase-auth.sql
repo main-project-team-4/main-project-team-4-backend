a9646edb780986838124aea4a48f6f3dcaff66d9
KANGCHANYOUNG <140377196+CHANYOUNGKANG@users.noreply.github.com>
2023-10-24 19:50:19 +0900

    Feat : "test 전체 삭제, aplplication.properties 삭제, 수정"

--- src/test/resources/data/testcase-auth.sql
+++
@@ -1,4 +0,0 @@
-insert into member (username, nickname) values (1, 'iksadnorth');
-insert into member (username, nickname) values (2, 'iksadsouth');
-insert into member (username, nickname) values (3, 'iksadeast');
-insert into member (username, nickname) values (4, 'iksadwest');
