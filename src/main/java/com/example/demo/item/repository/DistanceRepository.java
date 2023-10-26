2385768aa515a1affa7323b96e9167047ae5ee93
iksadNorth <iksadNorth@gmail.com>
2023-10-26 20:30:53 +0900

    "Fix : 내근처 상품에서 본인 상품 제외"
    내근처 상품에서 본인 상품 제외

--- src/main/java/com/example/demo/item/repository/DistanceRepository.java
+++ src/main/java/com/example/demo/item/repository/DistanceRepository.java
@@ -2,9 +2,10 @@ package com.example.demo.item.repository;
 
 import com.example.demo.item.entity.Item;
 import com.example.demo.location.entity.Location;
+import com.example.demo.member.entity.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface DistanceRepository {
-    Page<Item> findNearbyItems(Location location, Pageable pageable);
+    Page<Item> findNearbyItems(Location location, Member member, Pageable pageable);
 }
