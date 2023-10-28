8a165ed95dd7fc4aa316b5d4790ebc13b41f9c68
KANGCHANYOUNG <140377196+CHANYOUNGKANG@users.noreply.github.com>
2023-10-28 14:08:44 +0900

    FEAT : "CICD TEST deploy.yml 수정" "

--- script/run_new_process.sh
+++ script/run_new_process.sh
@@ -6,14 +6,12 @@
 CURRENT_PORT=$(cat /etc/nginx/conf.d/service-url.inc | grep -Po '[0-9]+' | tail -1)
 TARGET_PORT=0
 
-echo "> Current port of running WAS is ${CURRENT_PORT}."
-
 if [ ${CURRENT_PORT} -eq 8081 ]; then
   TARGET_PORT=8082
 elif [ ${CURRENT_PORT} -eq 8082 ]; then
   TARGET_PORT=8081
 else
-  echo "> No WAS is connected to nginx"
+  echo "> [$NOW_TIME] No WAS is connected to nginx"
 fi
 
 TARGET_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
@@ -22,9 +20,9 @@ if [ ! -z ${TARGET_PID} ]; then
   echo "> Kill WAS running at ${TARGET_PORT}."
   sudo kill ${TARGET_PID}
 fi
-JAR_NAME=$(ls -tr ./script/build/libs/demo-0.0.1-SNAPSHOT.jar | tail -n 1)
-echo "> JAR Name: $JAR_NAME"
-chmod +x $JAR_NAME
-nohup java -jar -Dserver.port=${TARGET_PORT} $JAR_NAME > /home/ubuntu/nohup.out 2>&1 &
+#JAR_NAME=$(ls -tr ./script/build/libs/demo-0.0.1-SNAPSHOT.jar | tail -n 1)
+#echo "> JAR Name: $JAR_NAME"
+#chmod +x $JAR_NAME
+nohup java -jar -Dserver.port=${TARGET_PORT} /home/ubuntu/*.jar
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
--- script/switch.sh
+++ script/switch.sh
@@ -3,7 +3,7 @@
 #!/bin/bash
 
 # Crawl current connected port of WAS
-CURRENT_PORT=$(cat /home/ubuntu/service_url.inc  | grep -Po '[0-9]+' | tail -1)
+CURRENT_PORT=$(cat /etc/nginx/conf.d/service-url.inc  | grep -Po '[0-9]+' | tail -1)
 TARGET_PORT=0
 
 echo "> Nginx currently proxies to ${CURRENT_PORT}."
@@ -19,7 +19,7 @@ else
 fi
 
 # Change proxying port into target port
-echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | tee /home/ubuntu/service_url.inc
+echo "set \$service_url http://3.200.8.55:${TARGET_PORT};" | tee /etc/nginx/conf.d/service-url.inc
 
 echo "> Now Nginx proxies to ${TARGET_PORT}."
 
