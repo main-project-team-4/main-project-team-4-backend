0eb57bfd8fd220dc5fc41ed8556ddd4d66a8d1c0
iksadNorth <iksadNorth@gmail.com>
2023-11-06 11:28:46 +0900

    "Fix : 상점 페이지 내의 팔로잉, 팔로워 목록 전달 시, 상점명 전달"
    상점 페이지 내의 팔로잉, 팔로워 목록 전달 시, 상점명 전달

--- src/main/java/com/example/demo/follow/dto/FollowMemberResponseDto.java
+++ src/main/java/com/example/demo/follow/dto/FollowMemberResponseDto.java
@@ -18,6 +18,9 @@ public class FollowMemberResponseDto {
     @Schema(description = "팔로우한 회원의 상점 id", example = "1")
     @JsonProperty(ParameterNameConfig.Shop.ID)
     private Long shopId;
+    @Schema(description = "팔로우한 회원의 상점 이름", example = "적절한 상점명")
+    @JsonProperty(ParameterNameConfig.Shop.NAME)
+    private String shopName;
     @Schema(description = "팔로우한 회원의 닉네임", example = "user1")
     @JsonProperty(ParameterNameConfig.Member.NICKNAME)
     private String nickname;
@@ -30,6 +33,9 @@ public class FollowMemberResponseDto {
         this.nickname = entity.getNickname();
         this.shopId = Optional.of(entity.getShop())
                 .map(Shop::getId)
+                .orElse(null);
+        this.shopName = Optional.of(entity.getShop())
+                .map(Shop::getShopName)
                 .orElse(null);
         this.imageUrl = Optional.of(entity)
                 .map(Member::getImage)
