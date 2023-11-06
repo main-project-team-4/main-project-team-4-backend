850bee54bc6ef016f55360ebe6000d7e8560b844
iksadNorth <iksadNorth@gmail.com>
2023-11-07 00:51:26 +0900

    "Feat : 이미지 버전 관리를 위한 추가적인 스크립트 작성"
    이미지 버전 관리를 위한 추가적인 스크립트 작성

--- script/build_docker_image.sh
+++ script/build_docker_image.sh
@@ -1,8 +1,15 @@
-TARGET_IMAGE=was-reuse:1.0.0;
-PUSH_IMAGE=iksadnorth/was-reuse:1.0.0;
+TARGET_IMAGE=was-reuse;
+PUSH_IMAGE=iksadnorth/was-reuse;
 DIR_DOCKERFILE=./;
 
+# Get Version Of Image
+VERSION_IMAGE=$(source ./script/count_image_version.sh)
+echo "This Version Of Image is $VERSION_IMAGE"
+
+# Generate Jar File With Gradle
+source ./script/build_spring_project.sh
+
 # Docker Build & Push
-docker build --tag $TARGET_IMAGE $DIR_DOCKERFILE
-docker tag $TARGET_IMAGE $PUSH_IMAGE
-docker push $PUSH_IMAGE
+docker build --tag $TARGET_IMAGE:$VERSION_IMAGE $DIR_DOCKERFILE
+docker tag $TARGET_IMAGE:$VERSION_IMAGE $PUSH_IMAGE:$VERSION_IMAGE
+docker push $PUSH_IMAGE:$VERSION_IMAGE
