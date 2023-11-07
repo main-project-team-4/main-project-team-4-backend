4be7e2cf877a11de760f1061c190180d9cb4ebae
iksadNorth <iksadNorth@gmail.com>
2023-11-07 15:25:53 +0900

    "Feat : 특정 상품에 대한 리뷰 목록 API 구현"
    특정 상품에 대한 리뷰 목록 API 구현

--- src/main/java/com/example/demo/review/repository/ReviewRepository.java
+++ src/main/java/com/example/demo/review/repository/ReviewRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ReviewRepository  extends JpaRepository<Review, Long> {
     Page<Review> findByShop_Id(Long shopId, Pageable pageable);
+    Optional<Review> findByItem_Id(Long itemId);
 }
