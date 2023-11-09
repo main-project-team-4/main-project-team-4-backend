ca49d58a1abaf6dedada853208e39b4d99882d82
iksadNorth <iksadNorth@gmail.com>
2023-11-09 11:14:53 +0900

    "Fix : Trade의 기록이 1개 이상일 때, 가장 최근의 기록 이용"
    Trade의 기록이 1개 이상일 때, 가장 최근의 기록 이용

--- src/main/java/com/example/demo/trade/repository/TradeRepository.java
+++ src/main/java/com/example/demo/trade/repository/TradeRepository.java
@@ -3,10 +3,11 @@ package com.example.demo.trade.repository;
 import com.example.demo.trade.entity.Trade;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TradeRepository extends JpaRepository<Trade, Long> {
     Optional<Trade> findByMember_IdAndItem_Id(Long memberId, Long itemId);
 
-    Optional<Trade> findByItem_Id(Long itemId);
+    List<Trade> findByItem_Id(Long itemId);
 }
