c7432bc571f7d3149801e9a65dc7c8532f6d9e8b
GukJang <9ukjang@gmail.com>
2023-11-11 23:16:34 +0900

    Temp : "객체 타입 확인용"

--- src/main/java/com/example/demo/chat/service/ChatMessageService.java
+++ src/main/java/com/example/demo/chat/service/ChatMessageService.java
@@ -49,9 +49,15 @@ public class ChatMessageService {
     public ChatMessageResponseDto sendMessages(ChatMessageRequestDto requestDto, Member member) {
         ChatMessage message = requestDto.toEntity();
 
+        log.info("프론트에서 날아온 dto 메세지 데이터 자체 : "+requestDto.getType());
+        log.info("프론트에서 날아온 dto 메세지 데이터 타입 : "+requestDto.getType().getClass().getName());
+        log.info("프론트에서 날아온 객체에 저장한 메세지 자체 : "+message.getType());
+        log.info("프론트에서 날아온 dto 메세지 데이터 타입 : "+message.getType().getClass().getName());
+
         // 퇴장 메세지
         if(MessageType.QUIT.equals(message.getType())){
-            return new ChatMessageResponseDto(message);
+            log.info("QUIT 인식함");
+            new ChatMessageResponseDto(message);
         }
 
         Member sender = memberRepository.findByNickname(message.getSender()).orElseThrow(() ->
